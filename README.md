6e646ac84ba5020cbdaa732ffc22832fa108db69
Mhayk Whandson <hi@mhayk.com>
2020-02-21 16:27:38 +0000

    A tool to edit/manipulate a svg files.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # svg-color-change
+
+A tool to edit svg files :}
---
+++ index.js
@@ -0,0 +1,31 @@
+var fs = require("fs");
+const path = require("path");
+const tools = require("simple-svg-tools");
+
+const { parse, stringify } = require("svgson");
+
+const directoryPath = path.join(__dirname, "./images");
+
+tools
+  .ImportDir(`${__dirname}/images/dark/`)
+  .then(collection => {
+    collection.forEach((svg, name) => {
+      console.log(`${name} >-> ${svg}`);
+
+      parse(svg.toString()).then(json => {
+        console.log(json.children[0].children[0].children[0].value);
+        json.children[0].children[0].children[0].value =
+          ".fa-secondary{opacity:.4;fill:#FFFFFF;}.fa-primary{fill:#ffffff;}";
+
+        const mysvg = stringify(json);
+        console.log(mysvg);
+
+        fs.writeFile(`${__dirname}/output/${name}.svg`, mysvg, err => {
+          if (err) throw err;
+
+          console.log(`${name} saved!`);
+        });
+      });
+    });
+  })
+  .catch(err => console.log(err));
