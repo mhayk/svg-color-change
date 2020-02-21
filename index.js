adf9a6ae28f3ca52e6764796df27ab690513c6f1
Mhayk Whandson <hi@mhayk.com>
2020-02-21 16:55:02 +0000

    The script has been updated to use async/await.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const { parse, stringify } = require("svgson");
 
 const directoryPath = path.join(__dirname, "./images");
 
+
+/*
 tools
   .ImportDir(`${__dirname}/images/dark/`)
   .then(collection => {
@@ -29,3 +31,25 @@ tools
     });
   })
   .catch(err => console.log(err));
+*/
+  async function main() {
+    try {
+      const collection = await tools.ImportDir(`${__dirname}/images/dark/`)
+
+      collection.forEach(async (svg, name) => {
+        const parsed = await parse(svg.toString())
+
+        parsed.children[0].children[0].children[0].value =
+          ".fa-secondary{opacity:.4;fill:#FFFFFF;}.fa-primary{fill:#ffffff;}";
+
+        const mysvg = stringify(parsed);
+
+        await fs.writeFileSync(`${__dirname}/output/${name}.svg`, mysvg)
+        console.log(`${name} saved!`);
+      })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+main()
